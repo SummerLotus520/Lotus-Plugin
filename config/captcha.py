da8bb9e4e8768db950547bd17f5b49b71d104d9f
SummerLotus520 <2402804043@qq.com>
2025-09-28 09:07:39 +0800

    fix:testnine地址，添加群成员列表，有bug请重新生成配置文件

--- config/captcha.py
+++ config/captcha.py
@@ -1,7 +1,7 @@
 import httpx
 def game_captcha(gt: str, challenge: str):
     print(f"gt: {gt}, challenge: {challenge}")
-    res = httpx.get("http://test:9645/pass_uni",params={'gt':gt,'challenge':challenge},timeout=10)
+    res = httpx.get("http://localhost:9645/pass_uni",params={'gt':gt,'challenge':challenge},timeout=10)
     print(res.text)
     datas = res.json()['data']
     if datas['result'] == 'success':
@@ -9,7 +9,7 @@ def game_captcha(gt: str, challenge: str):
     return None 
 def bbs_captcha(gt: str, challenge: str):
     print(f"gt: {gt}, challenge: {challenge}")
-    res = httpx.get("http://test:9645/pass_uni",params={'gt':gt,'challenge':challenge},timeout=10)
+    res = httpx.get("http://localhost:9645/pass_uni",params={'gt':gt,'challenge':challenge},timeout=10)
     print(res.text)
     datas = res.json()['data']
     if datas['result'] == 'success':
